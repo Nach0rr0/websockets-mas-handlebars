b6f07fa6c27261f590df04434decee1f0fe09749
Nach0rr0 <ignacio.roman@live.cl>
2024-01-17 14:48:07 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { Server } from "socket.io";
+import handlebars from "express-handlebars";
+import IndexRouter from "./routes/index.routes.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("src/public"));
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", "src/views");
+app.set("view engine", "handlebars");
+
+app.use((req, res, next) => {
+	req.io = io;
+	next();
+});
+
+app.use("/", IndexRouter);
+
+const server = app.listen(PORT, (error) => {
+	if (error) {
+		console.log(error);
+	}
+
+	console.log(`Servidor activo en localhost:${PORT}`);
+});
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+	console.log("Se conecto un nuevo ususario");
+});
